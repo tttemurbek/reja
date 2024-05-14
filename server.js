33a5c11acd385811ebb9db4a4049d51fea8a91bc
tttemurbek <nonemenoneme9999@gmail.com>
2024-05-14 23:49:03 +0900

    feat: we learnt about adding an ejs file to server.js file

--- server.js
+++ server.js
@@ -15,12 +15,13 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: rooting codes
-app.get("/hello", function (req, res) {
-  res.end("<h1>Asetullaev Temurbek Tolybekovich</h1>");
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
 
-app.get("/gift", function (req, res) {
-  res.end("<h1>Siz sovgalar bo'limidasiz</h1>");
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
@@ -28,4 +29,3 @@ let PORT = 3000;
 server.listen(PORT, function () {
   console.log(`The server is running successfully on port: ${PORT}`);
 });
-  
