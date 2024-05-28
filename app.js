78ea700c2ae313bb8fb2db9bea4255614b932c6d
tttemurbek <nonemenoneme9999@gmail.com>
2024-05-29 01:24:47 +0900

    feat: reja loyihamizining yakuniy bosqich logic

--- app.js
+++ app.js
@@ -52,6 +52,28 @@ app.post("/delete-item", (req, res) => {
   );
 });
 
+app.post("/edit-item", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  db.collection("plans").findOneAndUpdate(
+    {
+      _id: new mongodb.ObjectId(data.id),
+    },
+    { $set: { reja: data.new_input } },
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
+});
+
+app.post("/delete-all", (req, res) => {
+  if (req.body.delete_all) {
+    db.collection("plans").deleteMany(function () {
+      res.json({ state: "Everyting is deleted" });
+    });
+  }
+});
+
 app.get("/", function (req, res) {
   console.log("user entered /");
   db.collection("plans")
--- public/browser.js
+++ public/browser.js
@@ -1,3 +1,8 @@
+// const { default: axios } = require("axios");
+// const { response } = require("express");
+
+// const { default: axios } = require("axios");
+
 console.log("testing frontend javascript");
 
 function itemTemplate(item) {
@@ -59,6 +64,32 @@ document.addEventListener("click", function (e) {
   // edit oper
 
   if (e.target.classList.contains("edit-me")) {
-    alert("you pressed edit button");
+    const userInput = prompt(
+      "O'zgartirish kiriting",
+      e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+    );
+    if (userInput) {
+      axios
+        .post("/edit-item", {
+          id: e.target.getAttribute("data-id"),
+          new_input: userInput,
+        })
+        .then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.querySelector(
+            ".item-text"
+          ).innerHTML = userInput;
+        })
+        .catch((err) => {
+          console.log("Please, try again");
+        });
+    }
   }
+});
+
+document.getElementById("clean-all").addEventListener("click", function () {
+  axios.post("/delete-all", { delete_all: true }).then((response) => {
+    alert(response.data.state);
+    document.location.reload();
+  });
 });
